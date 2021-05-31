eff1b964fbca067ecfc7cbef13e0b382d6ad54f7
gunda <gundakamalnath@gmail.com>
2021-05-31 20:07:11 +0530

    li to be compared instead of movie div

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const movieList = document.querySelector(".movie-list");
-var movieListHeight = movieList.offsetHeight;
+
 const layout = document.querySelector(".movies");
-var layoutHeight = layout.offsetHeight;
-const layoutPos = layout.offsetTop;
-var threshold = layoutHeight/2;
-const movieCards = document.querySelectorAll(".movie");
+
+const movieCards = document.querySelectorAll(".list-item");
+
+var layoutPos = 0;
+var layoutHeight = 0;
+var threshold = 0;
+var movieListHeight = 0;
+
+window.addEventListener("DOMContentLoaded",() => {
+  layoutPos = layout.offsetTop;
+  layoutHeight = layout.offsetHeight;
+  threshold = layoutHeight/2;
+  movieListHeight = movieList.offsetHeight;
+  scrollHandler();
+  layout.addEventListener("scroll",scrollHandler);
+})
+
 const scrollHandler = () => {
     movieCards.forEach((item,index) => {
         let itemPos = item.getBoundingClientRect().top;
-        var scaleVal = 1 - Math.abs((threshold - itemPos)*0.5/layoutHeight);
+        var scaleVal = 1 - Math.abs((threshold - itemPos)*0.6/layoutHeight);
         item.style.transform = `scale(${scaleVal})`;
     })
 }
-layout.addEventListener("scroll",scrollHandler);
-movieList.onLoad = scrollHandler();
